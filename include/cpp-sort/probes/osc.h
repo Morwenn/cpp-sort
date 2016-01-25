a65c8f391b35388fb82b2c2582cc1c78b489fe02
Morwenn <morwenn29@hotmail.fr>
2016-01-25 21:57:36 +0100

    Add measure of presortedness: osc.

---
+++ include/cpp-sort/probes/osc.h
@@ -0,0 +1,101 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef CPPSORT_PROBES_OSC_H_
+#define CPPSORT_PROBES_OSC_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <type_traits>
+#include <cpp-sort/sorter_facade.h>
+#include <cpp-sort/sorter_traits.h>
+#include <cpp-sort/utility/as_function.h>
+#include <cpp-sort/utility/functional.h>
+#include "../detail/static_const.h"
+
+namespace cppsort
+{
+namespace probe
+{
+    namespace detail
+    {
+        struct osc_impl
+        {
+            template<
+                typename RandomAccessIterator,
+                typename Compare = std::less<>,
+                typename Projection = utility::identity,
+                typename = std::enable_if_t<
+                    is_projection_iterator<Projection, RandomAccessIterator, Compare>
+                >
+            >
+            auto operator()(RandomAccessIterator first, RandomAccessIterator last,
+                            Compare compare={}, Projection projection={}) const
+                -> typename std::iterator_traits<RandomAccessIterator>::difference_type
+            {
+                using difference_type = typename std::iterator_traits<RandomAccessIterator>::difference_type;
+                auto&& proj = utility::as_function(projection);
+
+                if (std::distance(first, last) < 2)
+                {
+                    return 0;
+                }
+
+                difference_type count = 0;
+                for (auto it = first ; it != last ; ++it)
+                {
+                    auto&& value = proj(*it);
+
+                    auto current = first;
+                    auto next = std::next(first);
+
+                    while (next != last)
+                    {
+                        if (compare(std::min(proj(*current), proj(*next)), value) &&
+                            compare(value, std::max(proj(*current), proj(*next))))
+                        {
+                            ++count;
+                        }
+
+                        ++current;
+                        ++next;
+                    }
+                }
+                return count;
+            }
+        };
+    }
+
+    namespace
+    {
+        constexpr auto&& osc = cppsort::detail::static_const<
+            sorter_facade<detail::osc_impl>
+        >::value;
+    }
+}}
+
+#endif // CPPSORT_PROBES_OSC_H_
