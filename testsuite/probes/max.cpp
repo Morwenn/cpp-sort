bee99071d5fe9bc684200c39fbae634ee5aafb66
Morwenn <morwenn29@hotmail.fr>
2016-01-26 20:10:50 +0100

    Add measure of presortedness: max.

--- testsuite/probes/max.cpp
+++ testsuite/probes/max.cpp
@@ -21,17 +21,34 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#ifndef CPPSORT_PROBES_H_
-#define CPPSORT_PROBES_H_
-
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
-#include <cpp-sort/probes/ham.h>
-#include <cpp-sort/probes/inv.h>
+#include <iterator>
+#include <vector>
+#include <catch.hpp>
 #include <cpp-sort/probes/max.h>
-#include <cpp-sort/probes/osc.h>
-#include <cpp-sort/probes/rem.h>
-#include <cpp-sort/probes/runs.h>
 
-#endif // CPPSORT_PROBES_H_
+TEST_CASE( "presortedness measure: max", "[probe][max]" )
+{
+    SECTION( "simple test" )
+    {
+        std::vector<int> vec = { 12, 28, 17, 59, 13, 10, 39, 21, 31, 30 };
+        CHECK( cppsort::probe::max(vec) == 6 );
+        CHECK( cppsort::probe::max(std::begin(vec), std::end(vec)) == 6 );
+    }
+
+    SECTION( "lower bound" )
+    {
+        std::vector<int> vec = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        CHECK( cppsort::probe::max(vec) == 0 );
+        CHECK( cppsort::probe::max(std::begin(vec), std::end(vec)) == 0 );
+    }
+
+    SECTION( "upper bound" )
+    {
+        // The upper bound should correspond to the size of
+        // the input sequence minus one
+
+        std::vector<int> vec = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        CHECK( cppsort::probe::max(vec) == 10 );
+        CHECK( cppsort::probe::max(std::begin(vec), std::end(vec)) == 10 );
+    }
+}
