20f348d73f99f2c3ced72feb1c6fbb4372484fda
Morwenn <morwenn29@hotmail.fr>
2016-01-29 16:57:57 +0100

    Improved support for projections.
    New features and loads of SFINAE: if a sorter implementation can't handle
    projection but handles arbitrary comparisons, sorter_facade sends it a
    specific comparison functions in which the projection operation has been
    merged.

---
+++ include/cpp-sort/detail/projection_compare.h
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef CPPSORT_DETAIL_PROJECTION_COMPARE_H_
+#define CPPSORT_DETAIL_PROJECTION_COMPARE_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <utility>
+#include <cpp-sort/sorter_traits.h>
+#include <cpp-sort/utility/as_function.h>
+
+namespace cppsort
+{
+namespace detail
+{
+    template<typename Compare, typename Projection>
+    class projection_compare
+    {
+        private:
+
+            using projection_t = decltype(utility::as_function(std::declval<Projection&>()));
+
+            Compare compare;
+            projection_t proj;
+
+        public:
+
+            projection_compare(Compare compare, Projection projection):
+                compare(compare),
+                proj(utility::as_function(projection))
+            {}
+
+            template<typename T, typename U>
+            auto operator()(T&& lhs, U&& rhs)
+                noexcept(noexcept(compare(proj(std::forward<T>(lhs)), proj(std::forward<U>(rhs)))))
+                -> decltype(compare(proj(std::forward<T>(lhs)), proj(std::forward<U>(rhs))))
+            {
+                return compare(proj(std::forward<T>(lhs)), proj(std::forward<U>(rhs)));
+            }
+    };
+
+    template<typename Compare, typename Projection>
+    auto make_projection_compare(Compare compare, Projection projection)
+        -> projection_compare<Compare, Projection>
+    {
+        return { compare, projection };
+    }
+}}
+
+#endif // CPPSORT_DETAIL_PROJECTION_COMPARE_H_
