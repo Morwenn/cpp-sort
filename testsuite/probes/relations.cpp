bf4ab92a1466107d8be43c1baf6d8cf55dd96741
Morwenn <morwenn29@hotmail.fr>
2016-02-13 18:34:41 +0100

    Test relations between measures of presortedness.

---
+++ testsuite/probes/relations.cpp
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <algorithm>
+#include <ctime>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <vector>
+#include <catch.hpp>
+#include <cpp-sort/probes.h>
+
+TEST_CASE( "relations between measures of presortedness", "[probe]" )
+{
+    std::vector<int> sequence(100);
+    std::iota(std::begin(sequence), std::end(sequence), 0);
+    std::mt19937 engine(std::time(nullptr));
+    std::shuffle(std::begin(sequence), std::end(sequence), engine);
+
+    // The computer science literature lists a number of
+    // relations between the results of different measures
+    // of presortedness on a same sequence; the following
+    // tests check that these relations are respected in
+    // the library
+
+    auto exc    = cppsort::probe::exc(sequence);
+    auto ham    = cppsort::probe::ham(sequence);
+    auto inv    = cppsort::probe::inv(sequence);
+    auto rem    = cppsort::probe::rem(sequence);
+    auto runs   = cppsort::probe::runs(sequence);
+
+    // Measures of Presortedness and Optimal Sorting Algorithms
+    // by Heikki Mannila
+    CHECK( exc <= inv );
+
+    // A framework for adaptative sorting by Ola Petersson and
+    // Alistair Moffat
+    CHECK( runs <= rem + 1 );
+    CHECK( exc + 1 <= ham );
+    CHECK( ham <= 2 * exc );
+}
