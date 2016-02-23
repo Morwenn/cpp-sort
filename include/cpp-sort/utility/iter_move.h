0be4239fe9e058552ebe03aa9a562ba725dbe4ff
Morwenn <morwenn29@hotmail.fr>
2016-02-23 23:25:58 +0100

    Add utility::iter_* customization point.
    Add functions utility::iter_swap and utility::iter_move as described by
    P0022: Proxy Iterators. These functions should be ADL-used so that users
    can provide alternative implementations for their own iterators.

---
+++ include/cpp-sort/utility/iter_move.h
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef CPPSORT_UTILITY_ITER_MOVE_H_
+#define CPPSORT_UTILITY_ITER_MOVE_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <type_traits>
+#include <utility>
+#include "../detail/iterator_traits.h"
+
+namespace cppsort
+{
+namespace utility
+{
+    namespace detail
+    {
+        template<typename Iterator>
+        using iter_move_t = std::conditional_t<
+            std::is_reference<cppsort::detail::reference_t<Iterator>>::value,
+            std::remove_reference_t<cppsort::detail::reference_t<Iterator>>&&,
+            std::decay_t<cppsort::detail::reference_t<Iterator>>
+        >;
+    }
+
+    template<typename Iterator>
+    auto iter_move(Iterator it)
+        noexcept(noexcept(detail::iter_move_t<Iterator>(std::move(*it))))
+        -> detail::iter_move_t<Iterator>
+    {
+        return std::move(*it);
+    }
+
+    template<typename Iterator>
+    auto iter_swap(Iterator lhs, Iterator rhs)
+        -> void
+    {
+        auto tmp = iter_move(lhs);
+        *lhs = iter_move(rhs);
+        *rhs = std::move(tmp);
+    }
+}}
+
+#endif // CPPSORT_UTILITY_ITER_MOVE_H_
