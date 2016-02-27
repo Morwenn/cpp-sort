47279b321674cb3b1c6a213a6e2326380fec6cf0
Morwenn <morwenn29@hotmail.fr>
2016-02-27 22:49:22 +0100

    Proxy iterators support for probe::inv.

--- include/cpp-sort/detail/count_inversions.h
+++ include/cpp-sort/detail/count_inversions.h
@@ -27,11 +27,11 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <algorithm>
 #include <iterator>
-#include <utility>
 #include <cpp-sort/utility/as_function.h>
+#include <cpp-sort/utility/iter_move.h>
 #include "iterator_traits.h"
+#include "move.h"
 
 namespace cppsort
 {
@@ -48,6 +48,8 @@ namespace detail
                                 Compare compare)
         -> ResultType
     {
+        using utility::iter_move;
+
         ResultType inversions = 0;
 
         // Shrink the problem size on the left side
@@ -62,19 +64,19 @@ namespace detail
         {
             if (first2 == last)
             {
-                std::move(first1, middle, result);
+                detail::move(first1, middle, result);
                 break;
             }
             if (compare(*first2, *first1))
             {
-                *result = std::move(*first2);
+                *result = iter_move(first2);
                 ++first2;
 
                 inversions += std::distance(first1, middle);
             }
             else
             {
-                *result = std::move(*first1);
+                *result = iter_move(first1);
                 ++first1;
             }
         }
@@ -82,7 +84,7 @@ namespace detail
         // Move everything back to the original array, note that
         // everything after first2 hasn't been moved to the cache
         // since it's already in its final place
-        std::move(cache, cache + std::distance(first, first2), first);
+        detail::move(cache, cache + std::distance(first, first2), first);
 
         return inversions;
     }
