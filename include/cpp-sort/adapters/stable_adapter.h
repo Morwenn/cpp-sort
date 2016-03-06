ab64f4a1f363c4e065cac7a1b9500552498168d7
Morwenn <morwenn29@hotmail.fr>
2016-03-06 23:48:51 +0100

    Add a stable_adapter to the library.
    This commit introduces a stable_adapter template, which takes a sorter and
    makes sure that it takes into account the original position of equivalent
    elements when it sorts the collection. To do so, it stores the original
    position of every element and compares it to decide which element should
    go first when two elements are equivalent. This version is suboptimal, but
    improvements will be added to optimize it when possible.

---
+++ include/cpp-sort/adapters/stable_adapter.h
@@ -0,0 +1,167 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef CPPSORT_ADAPTERS_STABLE_ADAPTER_H_
+#define CPPSORT_ADAPTERS_STABLE_ADAPTER_H_
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <cpp-sort/sorter_facade.h>
+#include <cpp-sort/sorter_traits.h>
+#include <cpp-sort/utility/functional.h>
+#include "../detail/associate_iterator.h"
+#include "../detail/checkers.h"
+#include "../detail/iterator_traits.h"
+#include "../detail/memory.h"
+
+namespace cppsort
+{
+    namespace detail
+    {
+        ////////////////////////////////////////////////////////////
+        // Stable comparison function
+
+        template<
+            typename Compare,
+            typename Projection = utility::identity
+        >
+        class stable_compare
+        {
+            private:
+
+                using projection_t = decltype(utility::as_function(std::declval<Projection&>()));
+                std::tuple<Compare, projection_t> data;
+
+            public:
+
+                stable_compare(Compare compare, Projection projection={}):
+                    data(compare, utility::as_function(projection))
+                {}
+
+                auto compare() const
+                    -> Compare
+                {
+                    return std::get<0>(data);
+                }
+
+                auto projection() const
+                    -> projection_t
+                {
+                    return std::get<1>(data);
+                }
+
+                template<typename T, typename U>
+                auto operator()(T&& lhs, U&& rhs)
+                    -> bool
+                {
+                    if (std::get<0>(data)(std::get<1>(data)(std::forward<T>(lhs).get()),
+                                          std::get<1>(data)(std::forward<U>(rhs).get())))
+                    {
+                        return true;
+                    }
+                    if (std::get<0>(data)(std::get<1>(data)(std::forward<U>(rhs).get()),
+                                          std::get<1>(data)(std::forward<T>(lhs).get())))
+                    {
+                        return false;
+                    }
+                    return lhs.data < rhs.data;
+                }
+        };
+
+        template<typename Compare, typename Projection=utility::identity>
+        auto make_stable_compare(Compare compare, Projection projection={})
+            -> stable_compare<Compare, Projection>
+        {
+            return { compare, projection };
+        }
+
+        ////////////////////////////////////////////////////////////
+        // Adapter
+
+        template<typename Sorter>
+        struct stable_adapter_impl:
+            check_iterator_category<Sorter>
+        {
+            template<
+                typename Iterator,
+                typename Compare = std::less<>,
+                typename Projection = utility::identity,
+                typename = std::enable_if_t<is_projection_iterator<
+                    Projection, Iterator, Compare
+                >>
+            >
+            auto operator()(Iterator first, Iterator last,
+                            Compare compare={}, Projection projection={}) const
+                -> decltype(auto)
+            {
+                using difference_type = difference_type_t<Iterator>;
+                using value_t = association<Iterator, difference_type>;
+
+                ////////////////////////////////////////////////////////////
+                // Bind index to iterator
+
+                auto size = std::distance(first, last);
+                std::unique_ptr<value_t, operator_deleter> iterators(
+                    static_cast<value_t*>(::operator new(size * sizeof(value_t)))
+                );
+                destruct_n d(0);
+                std::unique_ptr<value_t, destruct_n&> h2(iterators.get(), d);
+
+                // Associate iterators to their position
+                difference_type count = 0;
+                for (auto ptr = iterators.get() ; first != last ; ++first, (void) ++ptr)
+                {
+                    ::new(ptr) value_t(first, count++);
+                    d.incr((value_t*)nullptr);
+                }
+
+                ////////////////////////////////////////////////////////////
+                // Sort but takes the index into account to ensure stability
+
+                return Sorter{}(
+                    make_associate_iterator(iterators.get()),
+                    make_associate_iterator(iterators.get() + size),
+                    make_stable_compare(compare, projection)
+                );
+            }
+        };
+    }
+
+    template<typename Sorter>
+    struct stable_adapter:
+        std::conditional_t<
+            is_stable<Sorter>::value,
+            Sorter,
+            sorter_facade<detail::stable_adapter_impl<Sorter>>
+        >
+    {};
+}
+
+#endif // CPPSORT_ADAPTERS_STABLE_ADAPTER_H_
---
+++ testsuite/adapters/stable_adapter_every_sorter.cpp
@@ -0,0 +1,177 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Morwenn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <algorithm>
+#include <array>
+#include <ctime>
+#include <iterator>
+#include <random>
+#include <vector>
+#include <catch.hpp>
+#include <cpp-sort/adapters/stable_adapter.h>
+#include <cpp-sort/sort.h>
+#include <cpp-sort/sorters.h>
+#include <cpp-sort/utility/buffer.h>
+
+namespace
+{
+    struct wrapper
+    {
+        int value;
+        int order;
+    };
+
+    auto operator<(const wrapper& lhs, const wrapper& rhs)
+        -> bool
+    {
+        if (lhs.value < rhs.value)
+        {
+            return true;
+        }
+        if (rhs.value < lhs.value)
+        {
+            return false;
+        }
+        return lhs.order < rhs.order;
+    }
+}
+
+TEST_CASE( "every sorter with stable adapter",
+           "[stable_adapter]" )
+{
+    std::vector<wrapper> collection(412);
+    std::size_t count = 0;
+    for (wrapper& wrap: collection)
+    {
+        wrap.value = count++ % 17;
+    }
+    std::mt19937 engine(std::time(nullptr));
+    std::shuffle(std::begin(collection), std::end(collection), engine);
+    count = 0;
+    for (wrapper& wrap: collection)
+    {
+        wrap.order = count++;
+    }
+
+    SECTION( "block_sorter" )
+    {
+        using namespace cppsort;
+
+        using sorter = stable_adapter<block_sorter<
+            utility::fixed_buffer<0>
+        >>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "default_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::default_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "grail_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::grail_sorter<>>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "heap_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::heap_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "insertion_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::insertion_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "merge_insertion_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::merge_insertion_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "merge_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::merge_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "pdq_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::pdq_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "poplar_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::poplar_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "quick_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::quick_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "selection_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::selection_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "smooth_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::smooth_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "tim_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::tim_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+
+    SECTION( "verge_sorter" )
+    {
+        using sorter = cppsort::stable_adapter<cppsort::verge_sorter>;
+        cppsort::sort(collection, sorter{}, &wrapper::value);
+        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+    }
+}
