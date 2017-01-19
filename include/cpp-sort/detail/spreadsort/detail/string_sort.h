73fc3ba691f2a3dc32cdd1c0f99b1854ffeeedc5
Morwenn <morwenn29@hotmail.fr>
2017-01-19 22:10:20 +0100

    Fix tests for non-comparison sorters

--- include/cpp-sort/detail/spreadsort/detail/string_sort.h
+++ include/cpp-sort/detail/spreadsort/detail/string_sort.h
@@ -12,7 +12,7 @@ Some improvements suggested by:
 Phil Endecott and Frank Gennari
 */
 
-// Modified in 2015-2016 by Morwenn for inclusion into cpp-sort
+// Modified in 2015-2017 by Morwenn for inclusion into cpp-sort
 
 #ifndef CPPSORT_DETAIL_SPREADSORT_DETAIL_STRING_SORT_H_
 #define CPPSORT_DETAIL_SPREADSORT_DETAIL_STRING_SORT_H_
@@ -208,9 +208,9 @@ namespace spreadsort
           ++current) {
         //empties belong in this bin
         while (proj(*current).size() > char_offset) {
-          target_bin =
-            bins + static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
-          iter_swap(current, (*target_bin)++);
+          target_bin = bins + static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
+          iter_swap(current, *target_bin);
+          ++(*target_bin);
         }
       }
       *local_bin = next_bin_start;
@@ -227,9 +227,12 @@ namespace spreadsort
             ++current) {
           //Swapping into place until the correct element has been swapped in
           for (target_bin = bins + static_cast<Unsigned_char_type>
-              (proj(*current)[char_offset]);  target_bin != local_bin;
-            target_bin = bins + static_cast<Unsigned_char_type>
-              (proj(*current)[char_offset])) iter_swap(current, (*target_bin)++);
+               (proj(*current)[char_offset]); target_bin != local_bin;
+               target_bin = bins + static_cast<Unsigned_char_type>
+               (proj(*current)[char_offset])) {
+            iter_swap(current, *target_bin);
+            ++(*target_bin);
+          }
         }
         *local_bin = next_bin_start;
       }
@@ -317,9 +320,9 @@ namespace spreadsort
           ++current) {
         //empties belong in this bin
         while (proj(*current).size() > char_offset) {
-          target_bin =
-            end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
-          iter_swap(current, (*target_bin)++);
+          target_bin = end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
+          iter_swap(current, *target_bin);
+          ++(*target_bin);
         }
       }
       *local_bin = next_bin_start;
@@ -337,11 +340,13 @@ namespace spreadsort
             ++current) {
           //Swapping into place until the correct element has been swapped in
           for (target_bin =
-            end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
-            target_bin != local_bin;
-            target_bin =
-            end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset]))
-              iter_swap(current, (*target_bin)++);
+               end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset]);
+               target_bin != local_bin;
+               target_bin =
+               end_bin - static_cast<Unsigned_char_type>(proj(*current)[char_offset])) {
+            iter_swap(current, *target_bin);
+            ++(*target_bin);
+          }
         }
         *local_bin = next_bin_start;
       }
--- testsuite/every_sorter_no_post_iterator.cpp
+++ testsuite/every_sorter_no_post_iterator.cpp
@@ -52,9 +52,9 @@ TEST_CASE( "test every sorter with no_post_iterator", "[sorters]" )
         CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
     }
 
-    SECTION( "count_sorter" )
+    SECTION( "counting_sorter" )
     {
-        cppsort::sort(cppsort::selection_sorter{}, first, last);
+        cppsort::sort(cppsort::counting_sorter{}, first, last);
         CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
     }
 
@@ -126,8 +126,47 @@ TEST_CASE( "test every sorter with no_post_iterator", "[sorters]" )
 
     SECTION( "ska_sorter" )
     {
-        cppsort::sort(cppsort::selection_sorter{}, first, last);
+        cppsort::sort(cppsort::ska_sorter{}, first, last);
         CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+
+        std::vector<float> collection_float;
+        for (long i = 56.0f ; i < 366.0f ; ++i) {
+            collection_float.emplace_back(i);
+        }
+        std::shuffle(std::begin(collection_float), std::end(collection_float), engine);
+
+        // Iterators with no post-increment and no post-decrement
+        auto first_float = make_no_post_iterator(std::begin(collection_float));
+        auto last_float = make_no_post_iterator(std::end(collection_float));
+
+        cppsort::sort(cppsort::ska_sorter{}, first_float, last_float);
+        CHECK( std::is_sorted(std::begin(collection_float), std::end(collection_float)) );
+
+        std::vector<double> collection_double;
+        for (long i = 56.0 ; i < 366.0 ; ++i) {
+            collection_double.emplace_back(i);
+        }
+        std::shuffle(std::begin(collection_double), std::end(collection_double), engine);
+
+        // Iterators with no post-increment and no post-decrement
+        auto first_double = make_no_post_iterator(std::begin(collection_double));
+        auto last_double = make_no_post_iterator(std::end(collection_double));
+
+        cppsort::sort(cppsort::ska_sorter{}, first_double, last_double);
+        CHECK( std::is_sorted(std::begin(collection_double), std::end(collection_double)) );
+
+        std::vector<std::string> collection_str;
+        for (long i = 56 ; i < 366 ; ++i) {
+            collection_str.emplace_back(std::to_string(i));
+        }
+        std::shuffle(std::begin(collection_str), std::end(collection_str), engine);
+
+        // Iterators with no post-increment and no post-decrement
+        auto first_str = make_no_post_iterator(std::begin(collection_str));
+        auto last_str = make_no_post_iterator(std::end(collection_str));
+
+        cppsort::sort(cppsort::ska_sorter{}, first_str, last_str);
+        CHECK( std::is_sorted(std::begin(collection_str), std::end(collection_str)) );
     }
 
     SECTION( "smooth_sorter" )
@@ -138,14 +177,45 @@ TEST_CASE( "test every sorter with no_post_iterator", "[sorters]" )
 
     SECTION( "spread_sorter" )
     {
-        cppsort::sort(cppsort::selection_sorter{}, first, last);
+        cppsort::sort(cppsort::spread_sorter{}, first, last);
         CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
-    }
 
-    SECTION( "std_sorter" )
-    {
-        cppsort::sort(cppsort::std_sorter{}, first, last);
-        CHECK( std::is_sorted(std::begin(collection), std::end(collection)) );
+        std::vector<float> collection_float;
+        for (long i = 56.0f ; i < 366.0f ; ++i) {
+            collection_float.emplace_back(i);
+        }
+        std::shuffle(std::begin(collection_float), std::end(collection_float), engine);
+
+        auto first_float = make_no_post_iterator(std::begin(collection_float));
+        auto last_float = make_no_post_iterator(std::end(collection_float));
+
+        cppsort::sort(cppsort::spread_sorter{}, first_float, last_float);
+        CHECK( std::is_sorted(std::begin(collection_float), std::end(collection_float)) );
+
+        std::vector<double> collection_double;
+        for (long i = 56.0 ; i < 366.0 ; ++i) {
+            collection_double.emplace_back(i);
+        }
+        std::shuffle(std::begin(collection_double), std::end(collection_double), engine);
+
+        auto first_double = make_no_post_iterator(std::begin(collection_double));
+        auto last_double = make_no_post_iterator(std::end(collection_double));
+
+        cppsort::sort(cppsort::spread_sorter{}, first_double, last_double);
+        CHECK( std::is_sorted(std::begin(collection_double), std::end(collection_double)) );
+
+        std::vector<std::string> collection_str;
+        for (long i = 56 ; i < 366 ; ++i) {
+            collection_str.emplace_back(std::to_string(i));
+        }
+        std::shuffle(std::begin(collection_str), std::end(collection_str), engine);
+
+        // Iterators with no post-increment and no post-decrement
+        auto first_str = make_no_post_iterator(std::begin(collection_str));
+        auto last_str = make_no_post_iterator(std::end(collection_str));
+
+        cppsort::sort(cppsort::spread_sorter{}, first_str, last_str);
+        CHECK( std::is_sorted(std::begin(collection_str), std::end(collection_str)) );
     }
 
     SECTION( "tim_sorter" )
