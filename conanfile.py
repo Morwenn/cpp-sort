dabcfb843043930bb7407d02a871904d3cdd8a9a
Uilian Ries <uilianries@gmail.com>
2018-01-15 22:16:37 -0200

    Support Conan package (#121)
    - Added compiler validation for MSVC
    - Copied license file to package folder
    - Added Conan job on Travis CI
    - C++14 as required to build test_package
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,21 @@ from conans import ConanFile
 class CppSortConan(ConanFile):
     name = "cpp-sort"
     version = "1.0.0"
-    license = "MIT"
+    settings = "compiler"
+    license = "https://github.com/Morwenn/cpp-sort/blob/master/license.txt"
     url = "https://github.com/Morwenn/cpp-sort"
+    author = "Morwenn <morwenn29@hotmail.fr>"
     description = "Additional sorting algorithms & related tools"
     exports_sources = "include/*"
+    exports = "license.txt"
     no_copy_source = True
 
+    def configure(self):
+        if self.settings.compiler == "Visual Studio":
+            raise Exception("Visual Studio is not supported.")
+
     def package(self):
+        self.copy("license*", dst="licenses", ignore_case=True, keep_path=False)
         self.copy(pattern="*", src="include", dst="include")
 
     def package_id(self):
