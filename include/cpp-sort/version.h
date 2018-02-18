a5f4ff60a64c87a9d8bfe450e7b70a7037727b92
Morwenn <morwenn29@hotmail.fr>
2018-02-18 15:20:20 +0100

    Preparing release 1.1.0

--- include/cpp-sort/version.h
+++ include/cpp-sort/version.h
@@ -27,7 +27,7 @@
 // Semantic versioning macros
 
 #define CPPSORT_VERSION_MAJOR 1
-#define CPPSORT_VERSION_MINOR 0
+#define CPPSORT_VERSION_MINOR 1
 #define CPPSORT_VERSION_PATCH 0
 
 #endif // CPPSORT_VERSION_H_
