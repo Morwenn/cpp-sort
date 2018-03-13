5948b8b25129665c7956f6197ab5d53b2127cfc1
Morwenn <morwenn29@hotmail.fr>
2018-03-13 22:18:19 +0100

    Factor out some code in drop_merge_sort

--- include/cpp-sort/detail/drop_merge_sort.h
+++ include/cpp-sort/detail/drop_merge_sort.h
@@ -40,87 +40,10 @@ namespace detail
 {
     constexpr static bool double_comparison = true;
 
-    // with-trivial-copies version
-    template<typename BidirectionalIterator, typename Compare, typename Projection>
-    auto drop_merge_sort(BidirectionalIterator begin, BidirectionalIterator end,
-                         Compare compare, Projection projection,
-                         std::true_type)
-        -> void
-    {
-        auto size = std::distance(begin, end);
-        if (size < 2) return;
-
-        auto&& comp = utility::as_function(compare);
-        auto&& proj = utility::as_function(projection);
-
-        using difference_type = difference_type_t<BidirectionalIterator>;
-        using value_type = value_type_t<BidirectionalIterator>;
-        std::vector<value_type> dropped;
-
-        difference_type num_dropped_in_row = 0;
-        auto write = begin;
-        auto read = begin;
-
-        constexpr difference_type recency = 8;
-
-        while (read != end) {
-            if (begin != write && comp(proj(*read), proj(*std::prev(write)))) {
-
-                if (double_comparison && num_dropped_in_row == 0 && write != std::next(begin) &&
-                    not comp(proj(*read), proj(*std::prev(write, 2)))) {
-                    dropped.push_back(*std::prev(write));
-                    *std::prev(write) = *read;
-                    ++read;
-                    continue;
-                }
-
-                if (num_dropped_in_row < recency) {
-                    dropped.push_back(*read);
-                    ++read;
-                    ++num_dropped_in_row;
-                } else {
-                    for (difference_type i = 0 ; i < num_dropped_in_row ; ++i) {
-                        --read;
-                        dropped.pop_back();
-                    }
-
-                    --write;
-                    dropped.push_back(*write);
-
-                    num_dropped_in_row = 0;
-                }
-            } else {
-                *write = std::move(*read);
-                ++read;
-                ++write;
-                num_dropped_in_row = 0;
-            }
-        }
-
-        // Sort the dropped elements
-        pdqsort(std::begin(dropped), std::end(dropped), compare, projection);
-
-        auto back = end;
-
-        while (not dropped.empty()) {
-            auto& last_dropped = dropped.back();
-
-            while (begin != write && comp(proj(last_dropped), proj(*std::prev(write)))) {
-                --back;
-                --write;
-                *back = std::move(*write);
-            }
-            --back;
-            *back = std::move(last_dropped);
-            dropped.pop_back();
-        }
-    }
-
     // move-only version
     template<typename BidirectionalIterator, typename Compare, typename Projection>
     auto drop_merge_sort(BidirectionalIterator begin, BidirectionalIterator end,
-                         Compare compare, Projection projection,
-                         std::false_type)
+                         Compare compare, Projection projection)
         -> void
     {
         using utility::iter_move;
@@ -159,7 +82,12 @@ namespace detail
                 } else {
                     for (difference_type i = 0 ; i < num_dropped_in_row ; ++i) {
                         --read;
-                        *read = std::move(*std::prev(dropped.end()));
+                        if (not std::is_trivially_copyable<rvalue_reference>::value) {
+                            // If the value is trivially copyable, then it shouldn't have
+                            // been modified by the call to iter_move, and the original
+                            // value is still fully where it should be
+                            *read = std::move(*std::prev(dropped.end()));
+                        }
                         dropped.pop_back();
                     }
 
@@ -193,17 +121,6 @@ namespace detail
             *back = std::move(last_dropped);
             dropped.pop_back();
         }
-    }
-
-    template<typename BidirectionalIterator, typename Compare, typename Projection>
-    auto drop_merge_sort(BidirectionalIterator first, BidirectionalIterator last,
-                         Compare compare, Projection projection)
-        -> void
-    {
-        using value_type = value_type_t<BidirectionalIterator>;
-        drop_merge_sort(std::move(first), std::move(last),
-                        std::move(compare), std::move(projection),
-                        std::is_trivially_copyable<value_type>{});
     }
 }}
 
