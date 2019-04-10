249c3a0c9368faaf63efe02df43838f4e775c411
Morwenn <morwenn29@hotmail.fr>
2019-04-10 23:31:55 +0200

    Change conanfile.py to install with CMake
    [ci skip]

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,24 @@
-from conans import ConanFile
+from conans import CMake, ConanFile
 
 
 class CppSortConan(ConanFile):
     name = "cpp-sort"
     version = "1.4.0"
-    settings = "compiler"
     license = "https://github.com/Morwenn/cpp-sort/blob/master/license.txt"
     url = "https://github.com/Morwenn/cpp-sort"
     author = "Morwenn <morwenn29@hotmail.fr>"
     description = "Additional sorting algorithms & related tools"
-    exports_sources = "include/*"
+    exports_sources = ("include/*", "CMakeLists.txt", "cmake/*")
     exports = "license.txt"
     no_copy_source = True
 
-    def configure(self):
-        if self.settings.compiler == "Visual Studio":
-            raise Exception("Visual Studio is not supported.")
-
     def package(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTING"] = "OFF"
+        cmake.configure()
+        cmake.install()
+
         self.copy("license*", dst="licenses", ignore_case=True, keep_path=False)
-        self.copy(pattern="*", src="include", dst="include")
 
     def package_id(self):
         self.info.header_only()
