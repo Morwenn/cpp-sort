a42d90518c13eb0c8392ff563a5122d81f5a0cbc
Morwenn <morwenn29@hotmail.fr>
2019-07-26 23:59:47 +0200

    More better case_inisensitive_less and natural_less

--- testsuite/comparators/transparent_comparators.cpp
+++ testsuite/comparators/transparent_comparators.cpp
@@ -21,9 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#include <algorithm>
 #include <set>
 #include <string>
+#include <vector>
 #include <catch2/catch.hpp>
+#include <cpp-sort/comparators/case_insensitive_less.h>
+#include <cpp-sort/comparators/natural_less.h>
 #include <cpp-sort/comparators/total_less.h>
 
 namespace
@@ -32,11 +36,37 @@ namespace
     // from a string but is comparable with one
     struct string_wrapper
     {
-        string_wrapper(int value):
-            yay(std::to_string(value))
-        {}
+        std::vector<char> yay;
 
-        std::string yay;
+        string_wrapper(int value)
+        {
+            auto str = std::to_string(value);
+            std::copy(str.begin(), str.end(), std::back_inserter(yay));
+        }
+
+        auto begin()
+            -> decltype(yay.begin())
+        {
+            return yay.begin();
+        }
+
+        auto begin() const
+            -> decltype(yay.begin())
+        {
+            return yay.begin();
+        }
+
+        auto end()
+            -> decltype(yay.end())
+        {
+            return yay.end();
+        }
+
+        auto end() const
+            -> decltype(yay.end())
+        {
+            return yay.end();
+        }
     };
 
     auto total_less(const string_wrapper& lhs, const string_wrapper& rhs)
@@ -48,13 +78,39 @@ namespace
     auto total_less(const string_wrapper& lhs, const std::string& rhs)
         -> bool
     {
-        return lhs.yay < rhs;
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
     }
 
     auto total_less(const std::string& lhs, const string_wrapper& rhs)
         -> bool
     {
-        return lhs < rhs.yay;
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+
+    auto case_insensitive_less(const string_wrapper& lhs, const std::string& rhs)
+        -> bool
+    {
+        // Doesn't make sense, but...
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+
+    auto case_insensitive_less(const std::string& lhs, const string_wrapper& rhs)
+        -> bool
+    {
+        // Doesn't make sense, but...
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+
+    auto natural_less(const string_wrapper& lhs, const std::string& rhs)
+        -> bool
+    {
+        return cppsort::detail::natural_less(lhs, rhs);
+    }
+
+    auto natural_less(const std::string& lhs, const string_wrapper& rhs)
+        -> bool
+    {
+        return cppsort::detail::natural_less(lhs, rhs);
     }
 }
 
@@ -62,8 +118,27 @@ TEST_CASE( "Check that comparators work as transparent comparators" )
 {
     using namespace std::string_literals;
 
-    std::set<string_wrapper, std::decay_t<decltype(cppsort::total_less)>> strings = { 1, 2, 3, 4 };
+    SECTION( "total_less" )
+    {
+        std::set<string_wrapper, std::decay_t<decltype(cppsort::total_less)>> strings = { 1, 2, 3, 4 };
 
-    CHECK( strings.find("1"s) != strings.end() );
-    CHECK( strings.find("8"s) == strings.end() );
+        CHECK( strings.find("1"s) != strings.end() );
+        CHECK( strings.find("8"s) == strings.end() );
+    }
+
+    SECTION( "case_insensitive_less" )
+    {
+        std::set<string_wrapper, std::decay_t<decltype(cppsort::case_insensitive_less)>> strings = { 1, 2, 3, 4 };
+
+        CHECK( strings.find("1"s) != strings.end() );
+        CHECK( strings.find("8"s) == strings.end() );
+    }
+
+    SECTION( "natural_less" )
+    {
+        std::set<string_wrapper, std::decay_t<decltype(cppsort::natural_less)>> strings = { 1, 2, 3, 4 };
+
+        CHECK( strings.find("1"s) != strings.end() );
+        CHECK( strings.find("8"s) == strings.end() );
+    }
 }
