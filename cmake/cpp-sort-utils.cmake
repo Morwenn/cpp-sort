8278cd3edad14d4292d4911d3a0fb14a9000a13b
Morwenn <morwenn29@hotmail.fr>
2019-09-06 00:07:10 +0200

    Cleanup CMake files

---
+++ cmake/cpp-sort-utils.cmake
@@ -0,0 +1,10 @@
+# Add a selection of warnings to a target
+macro(cppsort_add_warnings target)
+    target_compile_options(${target} PRIVATE
+        -Wall -Wextra -Wcast-align -Winline -Wmissing-declarations -Wmissing-include-dirs
+        -Wnon-virtual-dtor -Wodr -Wpedantic -Wredundant-decls -Wundef -Wunreachable-code
+        $<$<CXX_COMPILER_ID:GNU>:-Wlogical-op -Wuseless-cast -Wzero-as-null-pointer-constant>
+        # The warning when initializing an std::array is just too much of a bother
+        $<$<CXX_COMPILER_ID:Clang>:-Wno-missing-braces>
+    )
+endmacro()
