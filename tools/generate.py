c3e817d54fd1337ef1fb02f51ff43898607798c8
Morwenn <morwenn29@hotmail.fr>
2020-07-11 21:16:49 +0200

    Tweak generate.py
    [ci skip]

--- tools/generate.py
+++ tools/generate.py
@@ -2,15 +2,18 @@ import sys
 
 
 def transform(line):
-    line = line.strip('[],\n SWAP(); ')
+    line = line.strip('[],\nSWAP();')
     if line:
-        x, y = map(int, line.split(', '))
+        x, y = map(int, line.split(','))
         return 'iter_swap_if(first + {0}u, first + {1}u, compare, projection);'.format(x, y)
 
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as source:
         for line in source:
+            line = line.replace(' ', '')
+            line = line.replace('(', '[')
+            line = line.replace(')', ']')
             pairs = line.split('],[')
             for pair in pairs:
                 res = transform(pair)
