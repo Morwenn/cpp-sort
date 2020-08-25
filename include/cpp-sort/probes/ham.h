2967a13b16b850d029ccf05fa425587c0fcbb8dc
Morwenn <morwenn29@hotmail.fr>
2020-08-25 23:33:26 +0200

    Various probes: don't recompute size when already known (#169)

--- include/cpp-sort/probes/ham.h
+++ include/cpp-sort/probes/ham.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016-2019 Morwenn
+ * Copyright (c) 2016-2020 Morwenn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -36,6 +36,7 @@
 #include <cpp-sort/sorter_traits.h>
 #include <cpp-sort/utility/as_function.h>
 #include <cpp-sort/utility/functional.h>
+#include <cpp-sort/utility/size.h>
 #include <cpp-sort/utility/static_const.h>
 #include "../detail/indirect_compare.h"
 #include "../detail/iterator_traits.h"
@@ -47,8 +48,71 @@ namespace probe
 {
     namespace detail
     {
+        template<typename ForwardIterator, typename Compare, typename Projection>
+        auto ham_probe_algo(ForwardIterator first, ForwardIterator last,
+                            cppsort::detail::difference_type_t<ForwardIterator> size,
+                            Compare compare, Projection projection)
+            -> ::cppsort::detail::difference_type_t<ForwardIterator>
+        {
+            using difference_type = ::cppsort::detail::difference_type_t<ForwardIterator>;
+            auto&& comp = utility::as_function(compare);
+            auto&& proj = utility::as_function(projection);
+
+            if (size < 2) {
+                return 0;
+            }
+
+            ////////////////////////////////////////////////////////////
+            // Indirectly sort the iterators
+
+            // Copy the iterators in a vector
+            std::vector<ForwardIterator> iterators;
+            iterators.reserve(size);
+            for (ForwardIterator it = first ; it != last ; ++it) {
+                iterators.push_back(it);
+            }
+
+            // Sort the iterators on pointed values
+            cppsort::detail::pdqsort(
+                iterators.begin(), iterators.end(),
+                cppsort::detail::indirect_compare<Compare, Projection>(std::move(compare),
+                                                                       std::move(projection)),
+                utility::identity{}
+            );
+
+            ////////////////////////////////////////////////////////////
+            // Count the number of values not in place
+
+            difference_type count = 0;
+            for (auto&& it: iterators) {
+                if (comp(proj(*first), proj(*it)) ||
+                    comp(proj(*it), proj(*first))) {
+                    ++count;
+                }
+                ++first;
+            }
+            return count;
+        }
+
         struct ham_impl
         {
+            template<
+                typename ForwardIterable,
+                typename Compare = std::less<>,
+                typename Projection = utility::identity,
+                typename = std::enable_if_t<
+                    is_projection_v<Projection, ForwardIterable, Compare>
+                >
+            >
+            auto operator()(ForwardIterable&& iterable,
+                            Compare compare={}, Projection projection={}) const
+                -> decltype(auto)
+            {
+                return ham_probe_algo(std::begin(iterable), std::end(iterable),
+                                      utility::size(iterable),
+                                      std::move(compare), std::move(projection));
+            }
+
             template<
                 typename ForwardIterator,
                 typename Compare = std::less<>,
@@ -59,47 +123,10 @@ namespace probe
             >
             auto operator()(ForwardIterator first, ForwardIterator last,
                             Compare compare={}, Projection projection={}) const
-                -> cppsort::detail::difference_type_t<ForwardIterator>
+                -> decltype(auto)
             {
-                using difference_type = cppsort::detail::difference_type_t<ForwardIterator>;
-                auto&& comp = utility::as_function(compare);
-                auto&& proj = utility::as_function(projection);
-
-                auto size = std::distance(first, last);
-                if (size < 2) {
-                    return 0;
-                }
-
-                ////////////////////////////////////////////////////////////
-                // Indirectly sort the iterators
-
-                // Copy the iterators in a vector
-                std::vector<ForwardIterator> iterators;
-                iterators.reserve(size);
-                for (ForwardIterator it = first ; it != last ; ++it) {
-                    iterators.push_back(it);
-                }
-
-                // Sort the iterators on pointed values
-                cppsort::detail::pdqsort(
-                    iterators.begin(), iterators.end(),
-                    cppsort::detail::indirect_compare<Compare, Projection>(std::move(compare),
-                                                                           std::move(projection)),
-                    utility::identity{}
-                );
-
-                ////////////////////////////////////////////////////////////
-                // Count the number of values not in place
-
-                difference_type count = 0;
-                for (auto&& it: iterators) {
-                    if (comp(proj(*first), proj(*it)) ||
-                        comp(proj(*it), proj(*first))) {
-                        ++count;
-                    }
-                    ++first;
-                }
-                return count;
+                return ham_probe_algo(first, last, std::distance(first, last),
+                                      std::move(compare), std::move(projection));
             }
         };
     }
--- include/cpp-sort/probes/max.h
+++ include/cpp-sort/probes/max.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016-2018 Morwenn
+ * Copyright (c) 2016-2020 Morwenn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -37,6 +37,7 @@
 #include <cpp-sort/sorter_traits.h>
 #include <cpp-sort/utility/as_function.h>
 #include <cpp-sort/utility/functional.h>
+#include <cpp-sort/utility/size.h>
 #include <cpp-sort/utility/static_const.h>
 #include "../detail/equal_range.h"
 #include "../detail/indirect_compare.h"
@@ -49,8 +50,84 @@ namespace probe
 {
     namespace detail
     {
+        template<typename ForwardIterator, typename Compare, typename Projection>
+        auto max_probe_algo(ForwardIterator first, ForwardIterator last,
+                            cppsort::detail::difference_type_t<ForwardIterator> size,
+                            Compare compare, Projection projection)
+            -> ::cppsort::detail::difference_type_t<ForwardIterator>
+        {
+            using difference_type = ::cppsort::detail::difference_type_t<ForwardIterator>;
+            auto&& proj = utility::as_function(projection);
+
+            if (size < 2) {
+                return 0;
+            }
+
+            ////////////////////////////////////////////////////////////
+            // Indirectly sort the iterators
+
+            // Copy the iterators in a vector
+            std::vector<ForwardIterator> iterators;
+            iterators.reserve(size);
+            for (ForwardIterator it = first ; it != last ; ++it) {
+                iterators.push_back(it);
+            }
+
+            // Sort the iterators on pointed values
+            cppsort::detail::pdqsort(
+                iterators.begin(), iterators.end(),
+                cppsort::detail::indirect_compare<Compare, Projection>(std::move(compare),
+                                                                       std::move(projection)),
+                utility::identity{}
+            );
+
+            ////////////////////////////////////////////////////////////
+            // Maximum distance an element has to travel in order to
+            // reach its sorted position
+
+            difference_type max_dist = 0;
+            difference_type it_pos = 0;
+            for (auto it = first ; it != last ; ++it) {
+                // Find the range where *first belongs once sorted
+                auto rng = cppsort::detail::equal_range(
+                    iterators.begin(), iterators.end(), proj(*it), compare,
+                    [&proj](const auto& iterator) {
+                        return proj(*iterator);
+                    });
+                auto pos_min = std::distance(iterators.begin(), rng.first);
+                auto pos_max = std::distance(iterators.begin(), rng.second);
+
+                // If *first isn't into one of its sorted positions, computed the closest
+                if (it_pos < pos_min) {
+                    max_dist = std::max(pos_min - it_pos, max_dist);
+                } else if (it_pos >= pos_max) {
+                    max_dist = std::max(it_pos - pos_max + 1, max_dist);
+                }
+
+                ++it_pos;
+            }
+            return max_dist;
+        }
+
         struct max_impl
         {
+            template<
+                typename ForwardIterable,
+                typename Compare = std::less<>,
+                typename Projection = utility::identity,
+                typename = std::enable_if_t<
+                    is_projection_v<Projection, ForwardIterable, Compare>
+                >
+            >
+            auto operator()(ForwardIterable&& iterable,
+                            Compare compare={}, Projection projection={}) const
+                -> decltype(auto)
+            {
+                return max_probe_algo(std::begin(iterable), std::end(iterable),
+                                      utility::size(iterable),
+                                      std::move(compare), std::move(projection));
+            }
+
             template<
                 typename ForwardIterator,
                 typename Compare = std::less<>,
@@ -61,60 +138,10 @@ namespace probe
             >
             auto operator()(ForwardIterator first, ForwardIterator last,
                             Compare compare={}, Projection projection={}) const
-                -> cppsort::detail::difference_type_t<ForwardIterator>
+                -> decltype(auto)
             {
-                using difference_type = cppsort::detail::difference_type_t<ForwardIterator>;
-                auto&& proj = utility::as_function(projection);
-
-                auto size = std::distance(first, last);
-                if (size < 2) {
-                    return 0;
-                }
-
-                ////////////////////////////////////////////////////////////
-                // Indirectly sort the iterators
-
-                // Copy the iterators in a vector
-                std::vector<ForwardIterator> iterators;
-                iterators.reserve(size);
-                for (ForwardIterator it = first ; it != last ; ++it) {
-                    iterators.push_back(it);
-                }
-
-                // Sort the iterators on pointed values
-                cppsort::detail::pdqsort(
-                    iterators.begin(), iterators.end(),
-                    cppsort::detail::indirect_compare<Compare, Projection>(std::move(compare),
-                                                                           std::move(projection)),
-                    utility::identity{}
-                );
-
-                ////////////////////////////////////////////////////////////
-                // Maximum distance an element has to travel in order to
-                // reach its sorted position
-
-                difference_type max_dist = 0;
-                difference_type it_pos = 0;
-                for (auto it = first ; it != last ; ++it) {
-                    // Find the range where *first belongs once sorted
-                    auto rng = cppsort::detail::equal_range(
-                        iterators.begin(), iterators.end(), proj(*it), compare,
-                        [&proj](const auto& iterator) {
-                            return proj(*iterator);
-                        });
-                    auto pos_min = std::distance(iterators.begin(), rng.first);
-                    auto pos_max = std::distance(iterators.begin(), rng.second);
-
-                    // If *first isn't into one of its sorted positions, computed the closest
-                    if (it_pos < pos_min) {
-                        max_dist = std::max(pos_min - it_pos, max_dist);
-                    } else if (it_pos >= pos_max) {
-                        max_dist = std::max(it_pos - pos_max + 1, max_dist);
-                    }
-
-                    ++it_pos;
-                }
-                return max_dist;
+                return max_probe_algo(first, last, std::distance(first, last),
+                                      std::move(compare), std::move(projection));
             }
         };
     }
