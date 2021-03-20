a3283e1438339ee5a28661550467eb66d1493f97
Morwenn <morwenn29@hotmail.fr>
2021-03-20 14:55:44 +0100

    Remove accidentally committed test code

--- include/cpp-sort/sorters/slab_sorter.h
+++ include/cpp-sort/sorters/slab_sorter.h
@@ -73,66 +73,6 @@ namespace cppsort
         constexpr auto&& slab_sort
             = utility::static_const<slab_sorter>::value;
     }
-
-
-
-
-
-
-
-
-
-    ////////////////////////////////////////////////////////////
-    // Sorter
-
-    namespace detail
-    {
-        struct slab_sorter_impl2
-        {
-            template<
-                typename RandomAccessIterator,
-                typename Compare = std::less<>,
-                typename Projection = utility::identity,
-                typename = std::enable_if_t<
-                    is_projection_iterator_v<Projection, RandomAccessIterator, Compare>
-                >
-            >
-            auto operator()(RandomAccessIterator first, RandomAccessIterator last,
-                            Compare compare={}, Projection projection={}) const
-                -> void
-            {
-                static_assert(
-                    std::is_base_of<
-                        std::random_access_iterator_tag,
-                        iterator_category_t<RandomAccessIterator>
-                    >::value,
-                    "slab_sorter2 requires at least random-access iterators"
-                );
-
-                slabsort2(std::move(first), std::move(last),
-                         std::move(compare), std::move(projection));
-            }
-
-            ////////////////////////////////////////////////////////////
-            // Sorter traits
-
-            using iterator_category = std::random_access_iterator_tag;
-            using is_always_stable = std::false_type;
-        };
-    }
-
-    struct slab_sorter2:
-        sorter_facade<detail::slab_sorter_impl2>
-    {};
-
-    ////////////////////////////////////////////////////////////
-    // Sort function
-
-    namespace
-    {
-        constexpr auto&& slab_sort2
-            = utility::static_const<slab_sorter2>::value;
-    }
 }
 
 #endif // CPPSORT_SORTERS_SLAB_SORTER_H_
