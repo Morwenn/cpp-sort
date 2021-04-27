e8634efcda4c52fd40e6c395bab475aaf4d583a8
Morwenn <morwenn29@hotmail.fr>
2021-04-27 17:01:02 +0200

    Reduce template instantiations in is_in_pack

--- include/cpp-sort/detail/type_traits.h
+++ include/cpp-sort/detail/type_traits.h
@@ -296,29 +296,24 @@ namespace detail
     // is_in_pack: check whether a given std::size_t value
     // appears in a std::size_t... parameter pack
 
-    template<std::size_t Value, std::size_t... Values>
-    struct is_in_pack_impl;
+    template<std::size_t... Values>
+    constexpr auto is_in_pack_impl(std::size_t value) noexcept
+        -> bool
+    {
+        std::size_t arr[] = { Values... };
+        for (std::size_t val: arr) {
+            if (val == value) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    template<
-        std::size_t Value,
-        std::size_t Head,
-        std::size_t... Tail
-    >
-    struct is_in_pack_impl<Value, Head, Tail...>:
-        conditional_t<
-            Value == Head,
-            std::true_type,
-            is_in_pack_impl<Value, Tail...>
-        >
-    {};
+    template<std::size_t Value, std::size_t... Values>
+    constexpr bool is_in_pack = is_in_pack_impl<Values...>(Value);
 
     template<std::size_t Value>
-    struct is_in_pack_impl<Value>:
-        std::false_type
-    {};
-
-    template<std::size_t Value, std::size_t... Values>
-    constexpr bool is_in_pack = is_in_pack_impl<Value, Values...>::value;
+    constexpr bool is_in_pack<Value> = false;
 }}
 
 #endif // CPPSORT_DETAIL_TYPE_TRAITS_H_
