fef948dfbdc2e38c7674b3cea5b6f4af4142a55c
Morwenn <morwenn29@hotmail.fr>
2021-10-31 18:48:57 +0100

    Test stable_adapter<verge_adapter> with every sorter

--- testsuite/adapters/mixed_adapters.cpp
+++ testsuite/adapters/mixed_adapters.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 Morwenn
+ * Copyright (c) 2016-2021 Morwenn
  * SPDX-License-Identifier: MIT
  */
 #include <algorithm>
@@ -13,7 +13,6 @@
 #include <cpp-sort/adapters.h>
 #include <cpp-sort/sorter_facade.h>
 #include <cpp-sort/sorter_traits.h>
-#include <cpp-sort/sorters/grail_sorter.h>
 #include <cpp-sort/sorters/insertion_sorter.h>
 #include <cpp-sort/sorters/poplar_sorter.h>
 #include <cpp-sort/sorters/selection_sorter.h>
@@ -203,23 +202,4 @@ TEST_CASE( "stability of counting_adapter over self_sort_adapter",
         CHECK( cppsort::is_stable<sorter2(std::list<int>::iterator, std::list<int>::iterator, std::negate<>)>::value );
         CHECK( cppsort::is_stable<sorter2(std::vector<int>::iterator, std::vector<int>::iterator, std::negate<>)>::value );
     }
-}
-
-TEST_CASE( "stable_adapter over verge_adapter",
-           "[stable_adapter][verge_adapter]" )
-{
-    using wrapper = generic_stable_wrapper<int>;
-    std::vector<wrapper> collection;
-    auto distribution = dist::descending_plateau{};
-    distribution(std::back_inserter(collection), 400);
-    helpers::iota(collection.begin(), collection.end(), 0, &wrapper::order);
-
-    cppsort::stable_adapter<
-        cppsort::verge_adapter<
-            cppsort::grail_sorter<>
-        >
-    > sorter;
-
-    sorter(collection, &wrapper::value);
-    CHECK( helpers::is_sorted(collection.begin(), collection.end()) );
 }
