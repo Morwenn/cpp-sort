d1bc55cea8d69371fdd4e58a04650eec445d35d9
Morwenn <morwenn29@hotmail.fr>
2021-12-29 16:43:43 +0100

    Add tests for flip() and not_fn()

---
+++ testsuite/comparators/flip_not.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2021 Morwenn
+ * SPDX-License-Identifier: MIT
+ */
+#include <functional>
+#include <catch2/catch.hpp>
+#include <cpp-sort/comparators/flip.h>
+#include <cpp-sort/comparators/not_fn.h>
+
+TEST_CASE( "flip", "[comparison]" )
+{
+    SECTION( "flip" )
+    {
+        auto cmp = cppsort::flip(std::less<>{});
+        CHECK_FALSE( cmp(1, 2) );
+        CHECK( cmp(2, 1) );
+        CHECK_FALSE( cmp(2, 2) );
+    }
+
+    SECTION( "flip(flip)" )
+    {
+        auto cmp = cppsort::flip(cppsort::flip(std::less<>{}));
+        CHECK( cmp(1, 2) );
+        CHECK_FALSE( cmp(2, 1) );
+        CHECK_FALSE( cmp(2, 2) );
+    }
+}
+
+TEST_CASE( "not_fn", "[comparison]" )
+{
+    SECTION( "not_fn" )
+    {
+        auto cmp = cppsort::not_fn(std::less<>{});
+        CHECK_FALSE( cmp(1, 2) );
+        CHECK( cmp(2, 1) );
+        CHECK( cmp(2, 2) );
+    }
+
+    SECTION( "not_fn(not_fn)" )
+    {
+        auto cmp = cppsort::not_fn(cppsort::not_fn(std::less<>{}));
+        CHECK( cmp(1, 2) );
+        CHECK_FALSE( cmp(2, 1) );
+        CHECK_FALSE( cmp(2, 2) );
+    }
+}
+
+TEST_CASE( "mixed flip and not_fn", "[comparison]" )
+{
+    SECTION( "not_fn(flip)" )
+    {
+        auto cmp = cppsort::not_fn(cppsort::flip(std::less<>{}));
+        CHECK( cmp(1, 2) );
+        CHECK_FALSE( cmp(2, 1) );
+        CHECK( cmp(2, 2) );
+    }
+
+    SECTION( "flip(not_fn)" )
+    {
+        auto cmp = cppsort::flip(cppsort::not_fn(std::less<>{}));
+        CHECK( cmp(1, 2) );
+        CHECK_FALSE( cmp(2, 1) );
+        CHECK( cmp(2, 2) );
+    }
+
+    SECTION( "not_fn(flip(not_fn))" )
+    {
+        auto cmp = cppsort::not_fn(cppsort::flip(cppsort::not_fn(std::less<>{})));
+        CHECK_FALSE( cmp(1, 2) );
+        CHECK( cmp(2, 1) );
+        CHECK_FALSE( cmp(2, 2) );
+    }
+
+    SECTION( "flip(not_fn(flip))" )
+    {
+        auto cmp = cppsort::flip(cppsort::not_fn(cppsort::flip(std::less<>{})));
+        CHECK_FALSE( cmp(1, 2) );
+        CHECK( cmp(2, 1) );
+        CHECK( cmp(2, 2) );
+    }
+}
