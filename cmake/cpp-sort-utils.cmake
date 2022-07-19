2df7317baac3e5ad2acdb428574713cccdf4b2fa
Morwenn <morwenn29@hotmail.fr>
2022-07-20 00:38:33 +0200

    Remove -Wno-missing-braces when compiling with Clang
    -Wmissing-braces used to fire for QOL and valid use cases such as
    std::array, but it is less eager in newer versions of Clang.

--- cmake/cpp-sort-utils.cmake
+++ cmake/cpp-sort-utils.cmake
@@ -11,8 +11,6 @@ macro(cppsort_add_warnings target)
             -Wnon-virtual-dtor -Wodr -Wpedantic -Wredundant-decls -Wundef -Wunreachable-code
             -Wno-inline
             $<$<CXX_COMPILER_ID:GNU>:-Wlogical-op -Wuseless-cast -Wzero-as-null-pointer-constant>
-            # The warning when initializing an std::array is just too much of a bother
-            $<$<CXX_COMPILER_ID:Clang>:-Wno-missing-braces>
         )
     endif()
 endmacro()
