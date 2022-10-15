429344c01c30eb846cf108625ca3f2f7a47f8067
Morwenn <morwenn29@hotmail.fr>
2022-10-15 17:00:38 +0200

    Simplify implementation of split_sorter

--- include/cpp-sort/sorters/split_sorter.h
+++ include/cpp-sort/sorters/split_sorter.h
@@ -8,64 +8,31 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <functional>
 #include <iterator>
 #include <type_traits>
-#include <utility>
-#include <cpp-sort/sorter_facade.h>
-#include <cpp-sort/sorter_traits.h>
+#include <cpp-sort/adapters/split_adapter.h>
 #include <cpp-sort/sorters/pdq_sorter.h>
-#include <cpp-sort/utility/functional.h>
 #include <cpp-sort/utility/static_const.h>
-#include "../detail/iterator_traits.h"
-#include "../detail/split_sort.h"
-#include "../detail/type_traits.h"
 
 namespace cppsort
 {
     ////////////////////////////////////////////////////////////
     // Sorter
 
-    namespace detail
-    {
-        struct split_sorter_impl
-        {
-            template<
-                typename RandomAccessIterator,
-                typename Compare = std::less<>,
-                typename Projection = utility::identity,
-                typename = detail::enable_if_t<
-                    is_projection_iterator_v<Projection, RandomAccessIterator, Compare>
-                >
-            >
-            auto operator()(RandomAccessIterator first, RandomAccessIterator last,
-                            Compare compare={}, Projection projection={}) const
-                -> void
-            {
-                static_assert(
-                    std::is_base_of<
-                        iterator_category,
-                        iterator_category_t<RandomAccessIterator>
-                    >::value,
-                    "split_sorter requires at least random-access iterators"
-                );
-
-                split_sort(std::move(first), std::move(last),
-                           std::move(compare), std::move(projection),
-                           cppsort::pdq_sort);
-            }
-
-            ////////////////////////////////////////////////////////////
-            // Sorter traits
-
-            using iterator_category = std::random_access_iterator_tag;
-            using is_always_stable = std::false_type;
-        };
-    }
-
     struct split_sorter:
-        sorter_facade<detail::split_sorter_impl>
-    {};
+        split_adapter<pdq_sorter>
+    {
+        ////////////////////////////////////////////////////////////
+        // Construction
+
+        split_sorter() = default;
+
+        ////////////////////////////////////////////////////////////
+        // Sorter traits
+
+        // Force it for legibility
+        using iterator_category = std::random_access_iterator_tag;
+    };
 
     ////////////////////////////////////////////////////////////
     // Sort function
