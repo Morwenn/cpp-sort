2c1bfa653886ea91bce76ea3aa182122b06f2fe3
Morwenn <morwenn29@hotmail.fr>
2022-11-05 16:34:18 +0100

    drop_merge_adapter: fix misleading template parameter name

--- include/cpp-sort/adapters/drop_merge_adapter.h
+++ include/cpp-sort/adapters/drop_merge_adapter.h
@@ -37,21 +37,21 @@ namespace cppsort
             {}
 
             template<
-                typename ForwardIterator,
+                typename BidirectionalIterator,
                 typename Compare = std::less<>,
                 typename Projection = utility::identity,
                 typename = detail::enable_if_t<
-                    is_projection_iterator_v<Projection, ForwardIterator, Compare>
+                    is_projection_iterator_v<Projection, BidirectionalIterator, Compare>
                 >
             >
-            auto operator()(ForwardIterator first, ForwardIterator last,
+            auto operator()(BidirectionalIterator first, BidirectionalIterator last,
                             Compare compare={}, Projection projection={}) const
                 -> void
             {
                 static_assert(
                     std::is_base_of<
                         iterator_category,
-                        iterator_category_t<ForwardIterator>
+                        iterator_category_t<BidirectionalIterator>
                     >::value,
                     "drop_merge_adapter requires at least bidirectional iterators"
                 );
