0410eb1680a1387ae3adb10b59cec55ce87f6a16
Morwenn <morwenn29@hotmail.fr>
2022-11-05 15:37:59 +0100

    More Conan v2 forward compatibility

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,9 @@
 import os.path
 
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy
-from conans import tools
 
 required_conan_version = ">=1.50.0"
 
@@ -17,7 +17,7 @@ class CppSortConan(ConanFile):
     name = "cpp-sort"
     version = "1.13.2"
     description = "Additional sorting algorithms & related tools"
-    topics = "conan", "cpp-sort", "sorting", "algorithms"
+    topics = "sorting", "algorithms"
     url = "https://github.com/Morwenn/cpp-sort"
     homepage = url
     license = "MIT"
@@ -34,8 +34,8 @@ class CppSortConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def validate(self):
-        if self.info.settings.get_safe("compiler.cppstd"):
-            tools.check_min_cppstd(self, 14)
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, 14)
 
     def layout(self):
         cmake_layout(self)
