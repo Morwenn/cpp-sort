fefa7570dc4f1b6909d6bba7f7bc3de33d21e53b
Morwenn <morwenn29@hotmail.fr>
2023-08-14 13:03:26 +0200

    Benchmarks: tweak/clean/organize distributions

--- benchmarks/inversions/inv-bench.cpp
+++ benchmarks/inversions/inv-bench.cpp
@@ -87,7 +87,7 @@ int main(int argc, char* argv[])
 
         for (int idx = 0; idx <= 100; ++idx) {
             double factor = 0.01 * idx;
-            auto distribution = dist::inversions(factor);
+            auto distribution = dist::inv(factor);
 
             std::vector<std::uint64_t> cycles;
 
