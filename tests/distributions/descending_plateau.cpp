cf7a241591684652be91b264cb18495cdf407202
Morwenn <morwenn29@hotmail.fr>
2025-07-14 13:47:56 +0200

    New distributions test: descending_plateau

---
+++ tests/distributions/descending_plateau.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2025 Morwenn
+ * SPDX-License-Identifier: MIT
+ */
+#include <list>
+#include <vector>
+#include <catch2/catch_template_test_macros.hpp>
+#include <cpp-sort/sorters.h>
+#include <cpp-sort/utility/buffer.h>
+#include <cpp-sort/utility/functional.h>
+#include <testing-tools/distributions.h>
+#include "test_distribution.h"
+
+TEMPLATE_TEST_CASE( "test random-access sorters with descending_plateau distribution", "[distributions]",
+                    cppsort::adaptive_shivers_sorter,
+                    cppsort::cartesian_tree_sorter,
+                    cppsort::d_ary_heap_sorter<6>,
+                    cppsort::drop_merge_sorter,
+                    cppsort::grail_sorter<>,
+                    cppsort::grail_sorter<
+                        cppsort::utility::dynamic_buffer<cppsort::utility::sqrt>
+                    >,
+                    cppsort::heap_sorter,
+                    cppsort::mel_sorter,
+                    cppsort::merge_sorter,
+                    cppsort::pdq_sorter,
+                    cppsort::poplar_sorter,
+                    cppsort::quick_merge_sorter,
+                    cppsort::quick_sorter,
+                    cppsort::ska_sorter,
+                    cppsort::slab_sorter,
+                    cppsort::smooth_sorter,
+                    cppsort::spin_sorter,
+                    cppsort::splay_sorter,
+                    cppsort::split_sorter,
+                    cppsort::spread_sorter,
+                    cppsort::std_sorter,
+                    cppsort::tim_sorter,
+                    cppsort::verge_sorter,
+                    cppsort::wiki_sorter<>,
+                    cppsort::wiki_sorter<
+                        cppsort::utility::dynamic_buffer<cppsort::utility::half>
+                    > )
+{
+    std::vector<int> collection;
+    helpers::test_distribution<TestType>(collection, 10'000, dist::descending_plateau{});
+}
+
+TEMPLATE_TEST_CASE( "test bidirectional sorters with descending_plateau distribution", "[distributions]",
+                    cppsort::cartesian_tree_sorter,
+                    cppsort::drop_merge_sorter,
+                    cppsort::merge_sorter,
+                    cppsort::quick_merge_sorter,
+                    cppsort::quick_sorter,
+                    cppsort::slab_sorter,
+                    cppsort::splay_sorter,
+                    cppsort::verge_sorter )
+{
+    std::list<int> collection;
+    helpers::test_distribution<TestType>(collection, 1000, dist::descending_plateau{});
+}
