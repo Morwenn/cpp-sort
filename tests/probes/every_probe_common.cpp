bfaf2b944fd2097fd73a8df546e6ab86f6d8e385
Morwenn <morwenn29@hotmail.fr>
2025-08-31 22:28:11 +0200

    Test order isomorphism of some measures of disorder
    That property of some measures of disorder is highlighted by Valdimir
    Estivill-Castro in *Sorting and Measures of Disorder*.

--- tests/probes/every_probe_common.cpp
+++ tests/probes/every_probe_common.cpp
@@ -2,13 +2,16 @@
  * Copyright (c) 2021-2025 Morwenn
  * SPDX-License-Identifier: MIT
  */
+#include <algorithm>
 #include <numeric>
+#include <random>
 #include <type_traits>
 #include <vector>
 #include <catch2/catch_template_test_macros.hpp>
 #include <rapidcheck.h>
 #include <rapidcheck/catch.h>
 #include <cpp-sort/probes.h>
+#include "testing-tools/random.h"
 
 //
 // Common tests for measures of presortedness
@@ -159,5 +162,82 @@ TEMPLATE_TEST_CASE( "test M(aX) <= |X| + M(X) for most probes M", "[probe]",
             return true;
         }
         return mop(sequence) <= (size - 1) + mop(sequence.begin() + 1, sequence.end());
+    });
+}
+
+TEMPLATE_TEST_CASE( "test prefix monoticity", "[probe]",
+                    decltype(cppsort::probe::dis),
+                    decltype(cppsort::probe::enc),
+                    decltype(cppsort::probe::exc),
+                    decltype(cppsort::probe::ham),
+                    decltype(cppsort::probe::inv),
+                    decltype(cppsort::probe::max),
+                    decltype(cppsort::probe::rem),
+                    decltype(cppsort::probe::runs),
+                    decltype(cppsort::probe::spear),
+                    decltype(cppsort::probe::sus) )
+{
+    // Property formalized by Estivill-Castro in *Sorting and Measures of Disorder*
+    // The following probes don't satisfy it: Block, Mono, Osc
+
+    // Note: the original paper claims that Osc also satisfies this property,
+    // but it fails for X=⟨3, 0⟩ Y=⟨⟩ Z=⟨4, 2⟩
+
+    rc::prop("prefix monoticity", [](std::vector<int> sequence) {
+        using diff_t = std::vector<int>::difference_type;
+        using param_t = std::uniform_int_distribution<diff_t>::param_type;
+
+        auto size = static_cast<diff_t>(sequence.size());
+        if (size < 3) {
+            return true;
+        }
+
+        // Split the sequence into three consequent subsequences X, Y and Z
+        std::uniform_int_distribution<diff_t> dist;
+        auto x_begin = sequence.begin();
+        auto y_begin = x_begin + dist(hasard::engine(), param_t{0, size - 1});
+        auto z_begin = x_begin + dist(hasard::engine(), param_t{y_begin - x_begin, size - 1});
+
+        // Ensure that all elements of Z are greater than all elements of X and Y
+        std::nth_element(x_begin, z_begin, sequence.end());
+
+        std::decay_t<TestType> mop;
+        auto disorder_x = mop(x_begin, y_begin);
+        auto disorder_y = mop(y_begin, z_begin);
+        auto disorder_yz = mop(y_begin, sequence.end());
+        auto new_x_begin = std::rotate(x_begin, y_begin, z_begin);
+        auto disorder_xz = mop(new_x_begin, sequence.end());
+
+        return disorder_x <= disorder_y
+            ? disorder_xz <= disorder_yz
+            : disorder_yz <= disorder_xz;
+    });
+}
+
+TEMPLATE_TEST_CASE( "test M(XY) = M(X) + M(Y) if X <= Y for most probes M", "[probe]",
+                    decltype(cppsort::probe::exc),
+                    decltype(cppsort::probe::ham),
+                    decltype(cppsort::probe::inv),
+                    decltype(cppsort::probe::rem),
+                    decltype(cppsort::probe::runs),
+                    decltype(cppsort::probe::spear) )
+{
+    // Property formalized by Estivill-Castro in *Sorting and Measures of Disorder*
+    // Not all measures of presortedness satisfy it, but a lot do
+
+    rc::prop("M(XY) = M(X) + M(Y) if X ≤ Y", [](std::vector<int> sequence) {
+        using diff_t = std::vector<int>::difference_type;
+        using param_t = std::uniform_int_distribution<diff_t>::param_type;
+
+        // Split the sequence into two consequent subsequences X and Y
+        auto size = static_cast<diff_t>(sequence.size());
+        std::uniform_int_distribution<diff_t> dist;
+        auto x_begin = sequence.begin();
+        auto y_begin = x_begin + dist(hasard::engine(), param_t{0, size});
+        std::cout << "nooooo: " << (y_begin - x_begin) << std::endl;
+        std::nth_element(x_begin, y_begin, sequence.end());
+
+        std::decay_t<TestType> mop;
+        return mop(sequence) == mop(x_begin, y_begin) + mop(y_begin, sequence.end());
     });
 }
