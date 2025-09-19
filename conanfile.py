40185b0d3b938e57d34e414b36e02080146a8604
Morwenn <morwenn29@hotmail.fr>
2025-09-19 22:36:43 +0200

    Preparing release 2.0.0

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ required_conan_version = ">=1.50.0"
 
 class CppSortConan(ConanFile):
     name = "cpp-sort"
-    version = "1.17.0"
+    version = "2.0.0"
     description = "Sorting algorithms & related tools"
     license = "MIT"
     url = "https://github.com/Morwenn/cpp-sort"
--- include/cpp-sort/version.h
+++ include/cpp-sort/version.h
@@ -7,8 +7,8 @@
 
 // Semantic versioning macros
 
-#define CPPSORT_VERSION_MAJOR 1
-#define CPPSORT_VERSION_MINOR 17
+#define CPPSORT_VERSION_MAJOR 2
+#define CPPSORT_VERSION_MINOR 0
 #define CPPSORT_VERSION_PATCH 0
 
 #endif // CPPSORT_VERSION_H_
