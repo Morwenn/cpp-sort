363ba03a41d9686f19bf5a138f792d201ad270c4
Morwenn <morwenn29@hotmail.fr>
2025-10-15 20:07:51 +0200

    Base LDS and LNDS on longest increasing subsequence

--- include/cpp-sort/detail/longest_increasing_subsequence.h
+++ include/cpp-sort/detail/longest_increasing_subsequence.h
@@ -2,8 +2,8 @@
  * Copyright (c) 2021-2025 Morwenn
  * SPDX-License-Identifier: MIT
  */
-#ifndef CPPSORT_DETAIL_LONGEST_NON_DESCENDING_SUBSEQUENCE_H_
-#define CPPSORT_DETAIL_LONGEST_NON_DESCENDING_SUBSEQUENCE_H_
+#ifndef CPPSORT_DETAIL_LONGEST_INCREASING_SUBSEQUENCE_H_
+#define CPPSORT_DETAIL_LONGEST_INCREASING_SUBSEQUENCE_H_
 
 ////////////////////////////////////////////////////////////
 // Headers
@@ -15,13 +15,13 @@
 #include <cpp-sort/utility/as_function.h>
 #include <cpp-sort/utility/functional.h>
 #include "iterator_traits.h"
-#include "upper_bound.h"
+#include "lower_bound.h"
 
 namespace cppsort::detail
 {
-    // Longest non-decreasing subsequence, computed with an altered
+    // Longest increasing subsequence, computed with an altered
     // patience sorting algorithm - returns a pair containing the
-    // size of the LNDS and the size of the collection
+    // size of the LIS and the size of the collection
 
     template<
         bool RecomputeSize,
@@ -29,9 +29,9 @@ namespace cppsort::detail
         typename Compare,
         typename Projection
     >
-    auto longest_non_descending_subsequence(ForwardIterator first, ForwardIterator last,
-                                            difference_type_t<ForwardIterator> size,
-                                            Compare compare, Projection projection)
+    auto longest_increasing_subsequence(ForwardIterator first, ForwardIterator last,
+                                        difference_type_t<ForwardIterator> size,
+                                        Compare compare, Projection projection)
         -> std::pair<difference_type_t<ForwardIterator>, difference_type_t<ForwardIterator>>
     {
         constexpr bool is_random_access = std::is_base_of_v<
@@ -59,18 +59,18 @@ namespace cppsort::detail
         // Top (smaller) elements in patience sorting stacks
         std::vector<ForwardIterator> stack_tops;
 
-        while (first != last) {
-            auto it = detail::upper_bound(
+        do {
+            auto it = detail::lower_bound(
                 stack_tops.begin(), stack_tops.end(),
                 proj(*first), compare, utility::indirect{} | projection);
 
             if (it == stack_tops.end()) {
-                // The element is bigger than everything else,
+                // The element is strictly bigger than everything else,
                 // create a new "stack" to put it
                 stack_tops.emplace_back(first);
             } else {
-                // The element is strictly smaller than the top
-                // of a given stack, replace the stack top
+                // The element is strictly smaller than or equal to
+                // the top of a given stack, replace the stack top
                 *it = first;
             }
             ++first;
@@ -79,10 +79,10 @@ namespace cppsort::detail
                 // Compute the size as-we-go if iterators are not random-access
                 ++size;
             }
-        }
+        } while (first != last);
 
         return { stack_tops.size(), size };
     }
 }
 
-#endif // CPPSORT_DETAIL_LONGEST_NON_DESCENDING_SUBSEQUENCE_H_
+#endif // CPPSORT_DETAIL_LONGEST_INCREASING_SUBSEQUENCE_H_
